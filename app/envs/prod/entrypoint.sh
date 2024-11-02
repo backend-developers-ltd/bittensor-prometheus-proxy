c5b1fe7f274fe5dab505c5915c10a5dc04687790
Michał Nowacki <michal.nowacki@reef.pl>
2024-11-02 16:30:22 +0100

    don't wait for database in entrypoint if there is no database

--- app/envs/prod/entrypoint.sh
+++ app/envs/prod/entrypoint.sh
@@ -3,8 +3,11 @@
 # We assume that WORKDIR is defined in Dockerfile
 
 ./prometheus-cleanup.sh
-PROMETHEUS_EXPORT_MIGRATIONS=0 ./manage.py wait_for_database --timeout 10
-# this seems to be the only place to put this for AWS deployments to pick it up
-PROMETHEUS_EXPORT_MIGRATIONS=0 ./manage.py migrate
 
-gunicorn -c gunicorn.conf.py
+if [ ! -z "$DATABASE_POOL_URL" ] || [ ! -z "$DATABASE_URL" ]; then
+    PROMETHEUS_EXPORT_MIGRATIONS=0 ./manage.py wait_for_database --timeout 10
+    # this seems to be the only place to put this for AWS deployments to pick it up
+    PROMETHEUS_EXPORT_MIGRATIONS=0 ./manage.py migrate
+fi
+
+exec gunicorn -c gunicorn.conf.py
